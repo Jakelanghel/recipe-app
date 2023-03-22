122e9cbd81345f25e383950852d7cd4d741a8dec
Jake <jacobtlanghel@gmail.com>
2023-03-21 21:01:55 -0400

    add custom error handler

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const express = require("express");
 const app = express();
 const recipesRoutes = require("./routes/recipes");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
+
 // middleware
 app.use(express.json());
+
 // routes
 app.use("/api/v1/recipes", recipesRoutes);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 // port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log("Server is listening on port 3000"));
+    app.listen(port, console.log(`Server is listening on port ${port}`));
   } catch (error) {
     console.log(error);
   }
--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,5 +1,6 @@
 const Recipe = require("../models/Recipe");
-const asyncWrapper = require("../middleware/asyncWrapper");
+const asyncWrapper = require("../middleware/async-wrapper");
+const { createCustomError } = require("../errors/custom-error");
 
 const createRecipe = asyncWrapper(async (req, res) => {
   const recipe = await Recipe.create(req.body);
@@ -11,9 +12,12 @@ const getAllRecipes = asyncWrapper(async (req, res) => {
   res.status(200).json({ recipes });
 });
 
-const getRecipe = asyncWrapper(async (req, res) => {
+const getRecipe = asyncWrapper(async (req, res, next) => {
   const { id: recipeID } = req.params;
   const recipe = await Recipe.findOne({ _id: recipeID });
+  if (!recipe) {
+    return next(createCustomError(`No recipe with the id ${recipeID}`));
+  }
   res.status(200).json({ recipe });
 });
 
@@ -24,7 +28,7 @@ const updateRecipe = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!recipe) {
-    return res.status(404).json({ mgs: `No recipe with ID: ${recipeID}` });
+    return next(createCustomError(`No recipe with the id ${recipeID}`));
   }
   res.status(200).json({ recipe });
 });
@@ -33,7 +37,7 @@ const deleteRecipe = asyncWrapper(async (req, res) => {
   const { id: recipeID } = req.params;
   const recipe = await Recipe.findOneAndDelete({ _id: recipeID });
   if (!recipe) {
-    return res.status(404).json({ mgs: `No recipe with ID: ${recipeID}` });
+    return next(createCustomError(`No recipe with the id ${recipeID}`));
   }
   res.status(200).json({ recipe });
 });
